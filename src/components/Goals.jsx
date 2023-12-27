f7c9c01be9d6dc3e067bb7205a2c5039e2a342df
Quintana, Junior <jquintana9@gatech.edu>
2023-12-27 13:36:43 -0500

    mobile responsive init work

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -3,7 +3,7 @@ import GoalCard from './GoalCard'
 
 export default function Goals () {
   return (
-    <div class="flex flex-nowrap gap-4 font-mono text-white text-sm font-bold leading-6 rounded-lg">
+    <div class="md:flex flex-nowrap gap-4 font-mono text-white text-sm font-bold leading-6 rounded-lg">
         <GoalCard team={"Atlanta United"} competition={"Leagues Cup"} date={"Friday, July 21st 2023"}></GoalCard>
         <GoalCard team={"Atlanta United"} competition={"Leagues Cup"} date={"Friday, July 21st 2023"}></GoalCard>
         <GoalCard team={"Cruz Azul"} competition={"Leagues Cup"} date={"Friday, July 21st 2023"} instagramLink={"https://www.instagram.com/reel/Cu-5LcstGdr/"}></GoalCard>
