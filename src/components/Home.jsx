99a45e49fc492aa16121e38f2e14132611fbd8cf
Quintana, Junior <jquintana9@gatech.edu>
2024-01-20 19:53:56 -0500

    adding assets

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Timeline } from 'react-twitter-widgets'
 
 export default function Home () {
   return (
-    <div class="relative w-96 md:w-80 lg:w-72">    
+    <div class="relative w-96">    
         <Timeline
         dataSource={{
             sourceType: 'url',
